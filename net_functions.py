422f300fdb26913b3ee0e9d119c2fbcb2a03db14
Josh31416 <33935790+josh31416@users.noreply.github.com>
2020-03-15 13:34:30 +0100

    feat: Trained MM model and SK model for classification

--- net_functions.py
+++ net_functions.py
@@ -3,7 +3,7 @@ import pandas as pd
 import torch
 
 
-def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path):
+def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path, valid_loss_min):
     """
     Trains a model and saves the one with the best validation loss
     Params
@@ -16,7 +16,7 @@ def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path):
         save_path: path to save the model when validation loss improves
     """
     # Initialization for validation loss tracking
-    valid_loss_min = np.Inf
+#     valid_loss_min = np.Inf
     last_valid_loss = 0
     
     for epoch in range(1, n_epochs+1):
@@ -61,11 +61,12 @@ def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path):
         if valid_loss < valid_loss_min and epoch > 1:
             valid_loss_min = valid_loss
             torch.save(model.state_dict(), save_path)
-            print(f'Epoch: {epoch} \tTraining Loss: {train_loss:.6f} \tValidation Loss: {valid_loss:.6f} \tCheckpoint!')
+#             print(f'Epoch: {epoch} \tTraining Loss: {train_loss:.6f} \tValidation Loss: {valid_loss:.6f} \tCheckpoint!')
+            print(f'{epoch} \tTrain: {train_loss:.6f} \tValid: {valid_loss:.6f} \tCheckpoint!')
         else:
-            print(f'Epoch: {epoch} \tTraining Loss: {train_loss:.6f} \tValidation Loss: {valid_loss:.6f}')
+            print(f'{epoch} \tTrain: {train_loss:.6f} \tValid: {valid_loss:.6f}')
             
-    return
+    return valid_loss_min
     
     
 def test_to_csv(loaders, MM_model, SK_model, use_cuda):
@@ -75,7 +76,7 @@ def test_to_csv(loaders, MM_model, SK_model, use_cuda):
     task_2 = []
     
     MM_model.eval()
-#     SK_model.eval()
+    SK_model.eval()
     for batch_idx, (data, target, path) in enumerate(loaders['test']):
         if use_cuda:
             data, target = data.cuda(), target.cuda()
@@ -83,19 +84,20 @@ def test_to_csv(loaders, MM_model, SK_model, use_cuda):
         Ids.append(path)
         
         output = MM_model(data)
-        pred = output.data.max(1, keepdim=True)[1]
-#         _, pred = torch.max(output, 1)
+#         pred = output.data.max(1, keepdim=True)[1]
+        _, pred = torch.max(output, 1)
         task_1.append(pred.data.cpu().numpy())
         
-#         output = SK_model(data)
+        output = SK_model(data)
 #         pred = output.data.max(1, keepdim=True)[1]
-#         task_2.append(pred.data.cpu().numpy())
+        _, pred = torch.max(output, 1)
+        task_2.append(pred.data.cpu().numpy())
 
     df = pd.DataFrame({
         'Id': np.array(Ids).flatten(),
         'task_1': np.array(task_1).flatten(),
-        'task_2': np.zeros_like(np.array(task_1).flatten()),
-#         'task_2': np.array(task_2).flatten(),
+#         'task_2': np.zeros_like(np.array(task_1).flatten()),
+        'task_2': np.array(task_2).flatten(),
     }, columns= ['Id', 'task_1', 'task_2'])
     
     df.to_csv('predictions.csv', index=False)
